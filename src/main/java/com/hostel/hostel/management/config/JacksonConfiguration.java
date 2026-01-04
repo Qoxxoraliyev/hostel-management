7fd253d27ac9046803153a3831c7e9edfaf53618
Qoxxoraliyev <qoxxoraliyev.muhammadali2006@gmail.com>
2026-01-04 22:32:41 +0500

    created configuration files

---
+++ src/main/java/com/hostel/hostel/management/config/JacksonConfiguration.java
@@ -0,0 +1,43 @@
+package com.hostel.hostel.management.config;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module.Feature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.io.IOException;
+import java.time.LocalTime;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class JacksonConfiguration {
+
+    public JavaTimeModule javaTimeModule(){
+        final JavaTimeModule javaTime=new JavaTimeModule();
+        javaTime.addSerializer(
+                LocalTime.class,
+                new JsonSerializer<LocalTime>() {
+                    @Override
+                    public void serialize(LocalTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+                        gen.writeString(value.toString());
+                    }
+                }
+        );
+        return javaTime;
+    }
+
+    @Bean
+    public Jdk8Module jdk8TimeModule(){
+        return new Jdk8Module();
+    }
+
+    public Hibernate6Module hibernate6Module(){
+        return new Hibernate6Module().configure(Feature.SERIALIZE_IDENTIFIER_FOR_LAZY_NOT_LOADED_OBJECTS,true);
+    }
+
+
+}
